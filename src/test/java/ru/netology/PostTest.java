e21c9cae9fea39e33437d26e7f81add1c93026ef
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-05-11 15:32:55 +0700

    Test Pass

--- src/test/java/ru/netology/PostTest.java
+++ src/test/java/ru/netology/PostTest.java
@@ -20,7 +20,7 @@ public class PostTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
 
 
 
