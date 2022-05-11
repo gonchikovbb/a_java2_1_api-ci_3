4ee4ba4506f72c1acd1ea9f2a09eaa3461204729
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-05-11 12:12:08 +0700

    Failed

---
+++ src/test/java/ru/netology/PostTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostTest {
+    @Test
+    void shouldPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+
+
+
+    }
+
+}
